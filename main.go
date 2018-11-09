1660c6dd7ac071399be4562d95dff28782bfd00b
jiajun.chen <cjj9931207@gmail.com>
2018-11-09 18:36:43 +0800

    1

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/chenjiajun1207/GameServer/Log"
+	"github.com/chenjiajun1207/GameServer/Network/Protocol"
 )
 
 func main() {
 	log15.Info("test")
+	log15.Info("test", "test", protocol.PkgHeartbeat)
 }
